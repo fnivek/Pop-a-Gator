3e753919f218b7928b0212e129ee7f4ad0162da7
Kevin French <fnivek@gmail.com>
2016-06-22 22:53:23 -0400

    TEST: Try out game sir interface

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
  */
 #include "board.h"
 
+ #include "game_sir.h"
+
 
 // main funciton
 int main(void)
@@ -10,8 +12,55 @@ int main(void)
 	setup_board();
 
 	// Infinite loop
+	uint16_t result = 0xFFFF;
 	while(true)
 	{
+		result = read_bluetooth(1000);
+		if(result != 0xFFFF)
+		{
+			game_sir_new_data(result);
+			if(game_sir_get_btn_value(HOME_BTN_INDEX))
+			{
+				set_debug_led(DEBUG_ALL_LEDS);
+				continue;
+			}
+			else
+			{
+				clear_debug_led(DEBUG_ALL_LEDS);
+			}
 
+			if(game_sir_get_btn_value(A_INDEX))
+			{
+				set_debug_led(DEBUG_GREEN_LED);
+			}
+			else
+			{
+				clear_debug_led(DEBUG_GREEN_LED);
+			}
+			if(game_sir_get_btn_value(B_INDEX))
+			{
+				set_debug_led(DEBUG_RED_LED);
+			}
+			else
+			{
+				clear_debug_led(DEBUG_RED_LED);
+			}
+			if(game_sir_get_btn_value(X_INDEX))
+			{
+				set_debug_led(DEBUG_BLUE_LED);
+			}
+			else
+			{
+				clear_debug_led(DEBUG_BLUE_LED);
+			}
+			if(game_sir_get_btn_value(Y_INDEX))
+			{
+				set_debug_led(DEBUG_ORANGE_LED);
+			}
+			else
+			{
+				clear_debug_led(DEBUG_ORANGE_LED);
+			}
+		}
 	}
 }
